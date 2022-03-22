a7516c7f2f7c669edd4acb48b896f839a96a9b18
Nox <benaziz.ines@gmail.com>
2022-03-22 12:13:11 +0100

    lab finished

--- routes/drones.js
+++ routes/drones.js
@@ -2,12 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 // require the Drone model here
-const Drone = require('../models/Drone.model')
+const Drones = require('../models/Drone.model')
 
 router.get('/drones', async (req, res, next) => {
   // Iteration #2: List the drones
   try {
-    const drones = await Drone.find()
+    const drones = await Drones.find()
     res.render('drones/list', {drones})
   }
   catch(error) {
@@ -23,35 +23,53 @@ router.get('/drones/create', (req, res, next) => {
 router.post('/drones/create', async (req, res, next) => {
   // Iteration #3: Add a new drone
   try {
-    const {name, propellers, maxSpeed} = req.body
-    const droneToCreate = {name, propellers, maxSpeed}
-    const createdDrone = await Drone.create(droneToCreate)
+    const droneToCreate = req.body
+    await Drones.create(droneToCreate)
 
-    res.set({ 'Content-Type': 'application/json' })
-    res.send(JSON.stringify(createdDrone))
-
-    const drones = await Drone.find()
-  // show the result
-    res.render('drones/list', {drones})
+    // show the result
+    res.redirect('/drones')
   }
-  catch(error) {
-    res.render('drones/create-form')
+  catch {
+    res.redirect('/drones/create')
   }
 });
 
-router.get('/drones/:id/edit', (req, res, next) => {
+router.get('/drones/:id/edit', async (req, res, next) => {
+  try {
+    const drone = await Drones.findById(req.params.id)
   // Iteration #4: Update the drone
-  // ... your code here
+  res.render('drones/update-form', {drone})
+  }
+  catch {
+    next()
+  }
 });
 
-router.post('/drones/:id/edit', (req, res, next) => {
+router.post('/drones/:id/edit', async (req, res, next) => {
   // Iteration #4: Update the drone
-  // ... your code here
+  try {
+    const droneToUpdate = req.body
+    await Drones.findByIdAndUpdate(req.params.id, droneToUpdate)
+
+    // show the result
+    res.redirect('/drones')
+  }
+  catch {
+    res.redirect(`/drones/${req.params.id}/edit`)
+  }
 });
 
-router.post('/drones/:id/delete', (req, res, next) => {
+router.post('/drones/:id/delete',async (req, res, next) => {
   // Iteration #5: Delete the drone
-  // ... your code here
+    try {
+    await Drones.findByIdAndDelete(req.params.id)
+
+    // show the result
+    res.redirect('/drones')
+  }
+  catch {
+    res.redirect(`/drones/${req.params.id}/edit`)
+  }
 });
 
 module.exports = router;
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -3,14 +3,28 @@ require('../db');
 const Drones = require('../models/Drone.model');
 const mongoose = require('mongoose')
 
+const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost/lab-express-drones'
+
 const drones = [
     { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
     { name: "Racer 57", propellers: 4, maxSpeed: 20 },
     { name: "Courier 3000i", propellers: 6, maxSpeed: 18 }
 ]
 
+async function connectToDB() {
+  try {
+    await mongoose.connect(MONGO_URI)
+    console.log(`Connected to Mongo!`)
+  }
+  catch(err) {
+    console.log('Failed to connect to mongo', err)
+    throw err
+  }
+}
+
 async function seedDrones() {
     try {
+      await connectToDB()
       await Drones.deleteMany()
       await Drones.insertMany(drones)
       await mongoose.connection.close()
@@ -18,6 +32,6 @@ async function seedDrones() {
     catch(error) {
       console.log(error)
     }
-  }
+}
   
-  seedDrones()
+seedDrones()
